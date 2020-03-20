ec407f0bd9c7abbad1448c3f8562ab5f60393fef
Jason Young <jasonsyoung>
2020-03-20 08:16:27 -0700

    Fixing Key Error

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class KeywordQueryEventListener(EventListener):
             for i in liste.keys():
                 if liste[i]["COMMAND"] in o:
                     o[liste[i]["COMMAND"]].append(liste[i]["NAME"])
-                else:
+                elif "NAME" in liste[i]:
                     o[liste[i]["COMMAND"]] = [liste[i]["NAME"]]
             
                 
